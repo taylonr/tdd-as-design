db8cac47bd77d9aa5c377a3a2e6600d391483281
Nate Taylor <taylonr@gmail.com>
2014-09-06 13:58:31 -0500

    Adding mocha tests.

--- gruntfile.js
+++ gruntfile.js
@@ -3,7 +3,7 @@
 module.exports = function(grunt) {
 	// Unified Watch Object
 	var watchFiles = {
-		serverViews: ['app/views/**/*.*'], 
+		serverViews: ['app/views/**/*.*'],
 		serverJS: ['gruntfile.js', 'server.js', 'config/**/*.js', 'app/**/*.js'],
 		clientViews: ['public/modules/**/views/*.html'],
 		clientJS: ['public/js/*.js', 'public/modules/**/*.js'],
@@ -47,6 +47,10 @@ module.exports = function(grunt) {
 				options: {
 					livereload: true
 				}
+			},
+			mochaTests:{
+				files: watchFiles.mochaTests,
+				tasks: ['mochaTest']
 			}
 		},
 		jshint: {
@@ -115,6 +119,7 @@ module.exports = function(grunt) {
 		concurrent: {
 			default: ['nodemon', 'watch'],
 			debug: ['nodemon', 'watch', 'node-inspector'],
+			test: ['mochaTest', 'watch'],
 			options: {
 				logConcurrentOutput: true
 			}
@@ -138,7 +143,7 @@ module.exports = function(grunt) {
 		}
 	});
 
-	// Load NPM tasks 
+	// Load NPM tasks
 	require('load-grunt-tasks')(grunt);
 
 	// Making grunt default to force in order not to break the project.
@@ -164,6 +169,8 @@ module.exports = function(grunt) {
 
 	// Build task(s).
 	grunt.registerTask('build', ['lint', 'loadConfig', 'ngmin', 'uglify', 'cssmin']);
+
+	grunt.registerTask('demoTest', ['env:test', 'concurrent:test']);
 
 	// Test task.
 	grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
