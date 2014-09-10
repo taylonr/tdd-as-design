3c8deae4ad99f2dabb8bee5257b5456731538d1b
Nate Taylor <taylonr@gmail.com>
2014-09-09 21:30:37 -0500

    Refactored model.

--- app/models/event.js
+++ app/models/event.js
@@ -1,23 +1,34 @@
 'use strict';
 
 var mongoose = require('mongoose'),
+  _ = require('lodash'),
   Schema = mongoose.Schema,
   EventSchema = new Schema({
     ratings: []
   });
 
-EventSchema.pre('save', function(next){
+EventSchema.methods.getTotalRating = function(){
   var totalRatings = 0;
 
-  for (var i = 0; i < this.ratings.length; i++) {
-    totalRatings += this.ratings[i].rating;
-  }
+  _.each(this.ratings, function(item){
+    totalRatings += item.rating;
+  });
+
+  return totalRatings;
+};
+
+EventSchema.methods.calculateAverageRating = function(){
+  var totalRatings = this.getTotalRating();
 
   if(this.ratings.length > 0){
     this.averageRating = totalRatings / this.ratings.length;
   } else {
     this.averageRating = 0;
   }
+};
+
+EventSchema.pre('save', function(next){
+  this.calculateAverageRating();
 
   next();
 });
